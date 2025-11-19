d9c775bfbaf66bb8a4d911f58318ca2e287bd874
Your GitHub Name <s202249140@kfupm.edu.sa>
2025-11-19 11:20:18 +0300

    TODO1

--- 7-2-restful-api/server/index.js
+++ 7-2-restful-api/server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config();
+await connectDB(process.env.MONGO_URL);
 
 import { connectDB } from "./db.js";
 import { Song } from "./models/song.model.js";
@@ -17,11 +19,21 @@ await connectDB(process.env.MONGO_URL);
 // api/songs (Read all songs)
 
 
+// Get single song by id
+
+
+
 // api/songs (Insert song)
+
+
 
 // /api/songs/:id (Update song)
 
 
+
 // /api/songs/:id (Delete song)
 
+
+
 app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+
